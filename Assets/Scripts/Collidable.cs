03af06f592c8e5104a9d6aa014d01d7718be2439
Luca Trușcă <t.luca1152@gmail.com>
2022-11-20 19:56:27 +0200

    Create base Collidable class

---
+++ Assets/Scripts/Collidable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    public ContactFilter2D filter;
+
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void Update()
+    {
+        // Collision work
+        boxCollider.OverlapCollider(filter, hits);
+
+        for (var i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+            {
+                continue;
+            }
+
+            OnCollide(hits[i]);
+
+            // The array is not cleaned up, clean it up
+            hits[i] = null;
+        }
+    }
+
+    protected virtual void OnCollide(Collider2D collider)
+    {
+        Debug.Log(collider.name);
+    }
+}
