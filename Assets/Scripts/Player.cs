81adaafb9fc4220169ec1a18244ac6f56d753bfc
Luca Trușcă <t.luca1152@gmail.com>
2022-11-20 17:09:34 +0200

    Handle collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     private void Start()
@@ -30,7 +31,28 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        // Make this thing move
-        transform.Translate(moveDelta * Time.deltaTime) ;
+        // Make sure we can move in the Y direction by casting a box there first; if the box is null there, we can move
+        hit = Physics2D.BoxCast(
+            transform.position, boxCollider.size,
+            0, new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        // Make sure we can move in the Y direction by casting a box there first; if the box is null there, we can move
+        hit = Physics2D.BoxCast(
+            transform.position, boxCollider.size,
+            0, new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }
